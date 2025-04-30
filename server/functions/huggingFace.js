4071e1d42baf5f842cfa30734308de80d2e804be
MisterHunter666 <marcos93bolonese@gmail.com>
2025-04-29 21:11:27 -0300

    main

--- server/functions/huggingFace.js
+++ server/functions/huggingFace.js
@@ -12,8 +12,8 @@ export async function generate(prompt) {
         // Generar imagen desde Hugging Face
         
         const response =  await inference.textToImage({
-             provider: "replicate",
-             model:"black-forest-labs/Flux.1-dev",
+             provider: "novita",
+             model:"meta-llama/llama-3.2-1b-instruct",
              inputs: prompt
          })
 
