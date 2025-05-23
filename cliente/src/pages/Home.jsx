062672af9d0e7268deb3a7b395b736310757b3bd
MisterHunter666 <marcos93bolonese@gmail.com>
2025-05-23 17:41:12 -0300

    remove .env

--- cliente/src/pages/Home.jsx
+++ cliente/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
     setLoading(true);
     try {
       const response = await fetch(
-        `${process.env.REACT_APP_DALLE_FETCH_ROUTE}/api/v1/post?search=${encodeURIComponent(search)}&page=${pageNum}&limit=8`
+        `https://dall-e-t4la.onrender.com/api/v1/post?search=${encodeURIComponent(search)}&page=${pageNum}&limit=8`
       );
       const result = await response.json();
       if (response.ok) {
