df09754410505ce2afa436bc969d514225ece693
MisterHunter666 <marcos93bolonese@gmail.com>
2025-05-23 18:25:46 -0300

    remove consolelog CreatePost

--- cliente/src/pages/CreatePost.jsx
+++ cliente/src/pages/CreatePost.jsx
@@ -17,6 +17,7 @@ const CreatePost = () => {
 
   const apiUrl = import.meta.env.VITE_API_URL;
   console.log(apiUrl)
+  console.log(import.meta.env)
   const generateImage = async () => {
     if (form.prompt) {
       try {
